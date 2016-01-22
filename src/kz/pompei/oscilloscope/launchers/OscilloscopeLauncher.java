72b6576deb6c733ece3c2d0e877815e8fcf1731b
pompei <john.kolpakov.x@gmail.com>
2016-01-22 20:27:37 +0600

    Реализован запуск пустой формы

--- src/kz/pompei/oscilloscope/launchers/OscilloscopeLauncher.java
+++ src/kz/pompei/oscilloscope/launchers/OscilloscopeLauncher.java
@@ -1,7 +1,28 @@
 package kz.pompei.oscilloscope.launchers;
 
-public class OscilloscopeLauncher {
+import javax.swing.*;
+import java.awt.*;
+
+public class OscilloscopeLauncher extends JPanel {
   public static void main(String[] args) {
-    System.out.println("OK");
+    SwingUtilities.invokeLater(() -> {
+      JFrame mainFrame = new JFrame();
+      mainFrame.setLocation(500, 300);
+      mainFrame.setSize(800, 400);
+      mainFrame.setContentPane(new OscilloscopeLauncher());
+      mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+      mainFrame.setVisible(true);
+    });
+  }
+
+  @Override
+  public void paint(Graphics g) {
+    super.paint(g);
+    paintG2((Graphics2D) g);
+  }
+
+  private void paintG2(Graphics2D g) {
+    g.setColor(Color.RED);
+    g.drawLine(10, 10, 100, 100);
   }
 }
